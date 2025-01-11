7decb6c8f6c445a0030fc3eda47367b926fe9ff4
julien <julien.jsmb.fr>
2025-01-11 14:56:00 +0100

    fix : update profile

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,128 +1,129 @@
-import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { Admin, Client, User } from "./user.entity";
-import { CreateAdminDto } from "./dtos/create-admin.dto";
-import { Repository } from "typeorm";
+import {HttpException, HttpStatus, Injectable} from "@nestjs/common";
+import {InjectRepository} from "@nestjs/typeorm";
+import {Admin, Client, User} from "./user.entity";
+import {CreateAdminDto} from "./dtos/create-admin.dto";
+import {Repository} from "typeorm";
 import {UpdateUserDto, UserCreateDto, UserDto, UserLoginDto} from "./dtos/user.dto";
-import { RegisterDto } from "../auth/interface/register.dto";
+import {RegisterDto} from "../auth/interface/register.dto";
 import * as bcrypt from "bcrypt";
 import {plainToInstance} from "class-transformer";
 
 @Injectable()
 export class UserService {
-  constructor(
-    @InjectRepository(Admin)
-    private adminRepository: Repository<Admin>,
-    @InjectRepository(Client)
-    private clientRepository: Repository<Client>,
-    @InjectRepository(User)
-    private userRepository: Repository<User>,
-  ) {}
-
-  private validateEmail(email: string): boolean {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-  }
-
-  async createAdmin(createAdminDto: CreateAdminDto): Promise<Admin> {
-    const admin = this.adminRepository.create(createAdminDto);
-    return this.adminRepository.save(admin);
-  }
-
-
-  async findClientById(id: number): Promise<UserDto> {
-    const user  = await this.userRepository.findOne({
-      where: { id },
-    });
-    return plainToInstance(UserDto, user, { excludeExtraneousValues: true });
-  }
-
-  async findAllAdmins(): Promise<UserDto[]> {
-    const admins = await this.adminRepository.find();
-    return plainToInstance(UserDto, admins, { excludeExtraneousValues: true });
-  }
-
-  async findAllClients(): Promise<UserDto[]> {
-    const clients = await this.clientRepository.find();
-    return plainToInstance(UserDto, clients, { excludeExtraneousValues: true });
-  }
-
-
-
-  async createClient(userCreateDto: UserCreateDto): Promise<RegisterDto> {
-
-    if (
-      !userCreateDto.firstName ||
-      !userCreateDto.lastName ||
-      !userCreateDto.email ||
-      !userCreateDto.password
+    constructor(
+        @InjectRepository(Admin)
+        private adminRepository: Repository<Admin>,
+        @InjectRepository(Client)
+        private clientRepository: Repository<Client>,
+        @InjectRepository(User)
+        private userRepository: Repository<User>,
     ) {
-      throw new HttpException("Missing Fields", HttpStatus.BAD_REQUEST);
     }
 
-    if (!this.validateEmail(userCreateDto.email)) {
-      throw new HttpException("Invalid email format", HttpStatus.BAD_REQUEST);
+    private validateEmail(email: string): boolean {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
     }
 
-    const existingClient = await this.clientRepository.findOne({
-      where: { email: userCreateDto.email },
-    });
-
-    if (existingClient) {
-      throw new HttpException("User Already Exists", HttpStatus.CONFLICT);
+    async createAdmin(createAdminDto: CreateAdminDto): Promise<Admin> {
+        const admin = this.adminRepository.create(createAdminDto);
+        return this.adminRepository.save(admin);
     }
 
-    const client = this.clientRepository.create({
-      firstName: userCreateDto.firstName,
-      lastName: userCreateDto.lastName,
-      email: userCreateDto.email,
-      password: userCreateDto.password,
-    });
 
-    await this.clientRepository.save(client);
-    return {
-      message: "User registration successful",
-    };
-  }
-
-  async findByLogin(userLoginDto: UserLoginDto): Promise<User> {
-    if (!this.validateEmail(userLoginDto.email)) {
-      throw new HttpException("Invalid email format", HttpStatus.BAD_REQUEST);
+    async findClientById(id: number): Promise<UserDto> {
+        const user = await this.userRepository.findOne({
+            where: {id},
+        });
+        return plainToInstance(UserDto, user, {excludeExtraneousValues: true});
     }
 
-    const user = await this.userRepository.findOne({
-      where: { email: userLoginDto.email }
-    });
+    async findAllAdmins(): Promise<UserDto[]> {
+        const admins = await this.adminRepository.find();
+        return plainToInstance(UserDto, admins, {excludeExtraneousValues: true});
+    }
 
-    if (!user)
-      throw new HttpException("User not found", HttpStatus.BAD_REQUEST);
+    async findAllClients(): Promise<UserDto[]> {
+        const clients = await this.clientRepository.find();
+        return plainToInstance(UserDto, clients, {excludeExtraneousValues: true});
+    }
 
-    const areEqual = await bcrypt.compare(userLoginDto.password, user.password);
-    if (!areEqual)
-      throw new HttpException("Wrong Password", HttpStatus.BAD_REQUEST);
 
-    return user;
-  }
+    async createClient(userCreateDto: UserCreateDto): Promise<RegisterDto> {
 
-  async findOneById(id: number): Promise<User> {
-    const user = await this.userRepository.findOne({
-      where: {id},
-      relations: [],
-    });
+        if (
+            !userCreateDto.firstName ||
+            !userCreateDto.lastName ||
+            !userCreateDto.email ||
+            !userCreateDto.password
+        ) {
+            throw new HttpException("Missing Fields", HttpStatus.BAD_REQUEST);
+        }
 
-    if (!user) throw new HttpException("User not Found", HttpStatus.NOT_FOUND);
-    return user;
-  }
+        if (!this.validateEmail(userCreateDto.email)) {
+            throw new HttpException("Invalid email format", HttpStatus.BAD_REQUEST);
+        }
 
-  async updateProfile(id: number, data: UpdateUserDto): Promise<UserDto> {
+        const existingClient = await this.clientRepository.findOne({
+            where: {email: userCreateDto.email},
+        });
 
-    const user = await this.findOneById(id);
+        if (existingClient) {
+            throw new HttpException("User Already Exists", HttpStatus.CONFLICT);
+        }
 
-    user.lastName = data.lastName;
-    user.firstName = data.firstName;
-    if(data.password && data.password.trim() !== "")
-      user.password = data.password;
-    
-    return plainToInstance(UserDto, this.userRepository.save(user));
-  }
+        const client = this.clientRepository.create({
+            firstName: userCreateDto.firstName,
+            lastName: userCreateDto.lastName,
+            email: userCreateDto.email,
+            password: userCreateDto.password,
+        });
+
+        await this.clientRepository.save(client);
+        return {
+            message: "User registration successful",
+        };
+    }
+
+    async findByLogin(userLoginDto: UserLoginDto): Promise<User> {
+        if (!this.validateEmail(userLoginDto.email)) {
+            throw new HttpException("Invalid email format", HttpStatus.BAD_REQUEST);
+        }
+
+        const user = await this.userRepository.findOne({
+            where: {email: userLoginDto.email}
+        });
+
+        if (!user)
+            throw new HttpException("User not found", HttpStatus.BAD_REQUEST);
+
+        const areEqual = await bcrypt.compare(userLoginDto.password, user.password);
+        if (!areEqual)
+            throw new HttpException("Wrong Password", HttpStatus.BAD_REQUEST);
+
+        return user;
+    }
+
+    async findOneById(id: number): Promise<User> {
+        const user = await this.userRepository.findOne({
+            where: {id},
+            relations: [],
+        });
+
+        if (!user) throw new HttpException("User not Found", HttpStatus.NOT_FOUND);
+        return user;
+    }
+
+    async updateProfile(id: number, data: UpdateUserDto): Promise<UserDto> {
+
+        const user = await this.findOneById(id);
+
+        user.lastName = data.lastName;
+        user.firstName = data.firstName;
+        if (data.password && data.password.trim() !== "") {
+            user.password = data.password;
+            return plainToInstance(UserDto, this.userRepository.save(user))
+        }
+        return plainToInstance(UserDto, this.userRepository.update(user.id,{lastName: user.lastName, firstName: user.firstName}));
+    }
 }
